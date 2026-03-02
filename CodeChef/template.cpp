ed229717003503b8af36256134a020e0aa165ee4
Prashanth2902 <cbeprashanth@gmail.com>
2026-03-02 09:43:28 -0500

    template fix

--- CodeChef/template.cpp
+++ CodeChef/template.cpp
@@ -7,8 +7,8 @@ void solve() {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     int t;
     cin >> t;
     while(t--) {
